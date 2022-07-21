bc2e765b23c31dfeab6965bf4193fc6960d590b9
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-07-21 16:54:42 +0200

    Move Playwright-Axe testing code from jupyter/accessibility

---
+++ jupyter_accessibility/__main__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from .dodo import Main
+
+main = Main.run
+
+if __name__ == "__main__":
+    main()
