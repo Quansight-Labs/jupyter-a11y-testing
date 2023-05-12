fb3af7a34b23f753e052958a9015d820b47ad8d7
Gabriel Fouasnon <gabriel@fouasnon.com>
2023-05-12 13:25:13 +0200

    use iterator

--- testing/jupyterlab/tests/regression-tests/visible-focus-indicator-initial-page.test.ts
+++ testing/jupyterlab/tests/regression-tests/visible-focus-indicator-initial-page.test.ts
@@ -4,18 +4,41 @@
 import { ElementHandle, expect } from "@playwright/test";
 import { test } from "@jupyterlab/galata";
 
+async function* getNodesInFocusOrder(page) {
+  let start = await page.evaluateHandle(() =>  document.activeElement) as ElementHandle;
+  let node = start;
+  while (true) {
+    yield node;
+    await page.keyboard.press("Tab");
+    node = await page.evaluateHandle(() => document.activeElement) as ElementHandle;
+    if (await page.evaluate(([current, start]) => current === start, [node, start])) {
+      break;
+    }
+  }
+}
+
 test("should have visible focus indicator", async (
   { page }, testInfo
 ) => {
+  for await (const node of getNodesInFocusOrder(page)) {
+    const box = await node.boundingBox();
+    if (box === null) {
+      throw new Error("Could not get node bounding box");
+    }
+    const { x, y, width, height } = box;
+    const pad = 3; // this value is just from trial-and-error
+    const clip = {
+      x: x - pad,
+      y: y - pad,
+      width: pad + width + pad,
+      height: pad + height + pad
+    };
 
-  await page.keyboard.press("Tab");
-  let start = await page.evaluateHandle(() =>  document.activeElement) as ElementHandle;
-  let node = start;
+    const focus = await page.screenshot({ clip });
+    await node.evaluate((node) => (node as HTMLElement).blur());
 
-  while (true) {
-    const focus = await node.screenshot();
-    await page.keyboard.press("Tab");
-    const noFocus = await node.screenshot();
+    const noFocus = await page.screenshot({ clip });
+    await node.evaluate((node) => (node as HTMLElement).focus());
 
     await testInfo.attach("focussed", {
       body: focus,
@@ -29,10 +52,5 @@ test("should have visible focus indicator", async (
     expect(
       focus.equals(noFocus)
     ).toBe(false);
-
-    node = await page.evaluateHandle(() => document.activeElement) as ElementHandle;
-    if (await page.evaluate(([current, start]) => current === start, [node, start])) {
-      break;
-    }
   }
 });
