2dca0382d56a8a58247146c1dac4d0cac8c86a49
Tania Allard <taniar.allard@gmail.com>
2023-07-10 12:55:55 +0100

    :wrench: Use ruff instead of flake8

--- testing/jupyterlab/jupyter_server_config.py
+++ testing/jupyterlab/jupyter_server_config.py
@@ -1,6 +1,8 @@
 # Copyright (c) Jupyter Accessibility Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+"""Configuration for a Jupyter Server to be used for the tests."""
+
 from tempfile import mkdtemp
 
 # These settings modeled after https://github.com/jupyterlab/jupyterlab/blob/%40jupyterlab/galata%404.0.2/galata/jupyter_server_test_config.py
